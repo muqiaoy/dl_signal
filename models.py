b3fd77383915fc1d9fb6fa68180f8c5ba0d01fc8
Martin Qianli Ma <qianlima@brandeis.edu>
2019-01-17 12:21:18 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -61,6 +61,31 @@ class BiGRU(nn.Module):
         output = self.out(r_out) # output is batch_size*256*60
         return r_out, nn.functional.log_softmax(output, dim=2)
 
+def eval_BiGRU(data, label, model, num_classes, loss_func, name, path):
+    global device
+    with torch.no_grad():
+        model.eval()
+        data = Variable(torch.from_numpy(data).float()).to(device=device)
+        true_label = np.argmax(label, axis=2)
+        label = Variable(torch.from_numpy(true_label).long()).view(-1).to(device=device)  # -1
+        compressed_signal, output = model(data)
+        output = output.view(-1, num_classes)
+        l = loss_func(output, label).item()
+        pred = np.argmax(output.data.cpu().numpy(), axis=1)
+        acc = np.mean(pred == true_label.reshape(-1))
+        print("%s loss %f and acc %f " % (name, l, acc))
+        
+        #Confusion Matrix Calculator
+        cnf_matrix = confusion_matrix(true_label.reshape(-1), pred)
+        
+        #Normalize Confusion Matrix
+        cnf_matrix = cnf_matrix.astype('float') / cnf_matrix.sum(axis=1)[:, np.newaxis]
+        
+        save_path = os.path.join(path,'confusion_matrix_' + name)
+        np.save(save_path, cnf_matrix)
+    
+    return compressed_signal, l, acc
+
 def eval_BiRNN(data, label, model, num_classes, loss_func, name, path):
     global device
     with torch.no_grad():
